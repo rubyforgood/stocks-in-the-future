a11b2fee76e3f4ae4a8c3a84c5c766fb9b783958
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-29 13:45:41 -0400

    fix route, add test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "devise/sessions#new"
+  root "home#index"
 end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "sign up flow" do
+    it "works" do
+      visit "/users/sign_up"
+
+      fill_in "Email", with: "test123@test.com"
+      fill_in "Password", with: "test123"
+      fill_in "Password confirmation", with: "test123"
+
+      expect {click_on "Sign up"}.to change(User, :count).by(1)
+    end
+  end
+end
