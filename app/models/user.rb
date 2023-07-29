b87ef58f927bc028d9bd1d1472123b9eaae4733b
kristenking <kristenking0110@gmail.com>
2023-07-29 16:31:49 -0400

    update user model, add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_152416) do
+
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_202714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_152416) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", limit: 50
+    t.string "last_name", limit: 50
+    t.string "username", limit: 50
+    t.boolean "active", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
