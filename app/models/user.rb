a1b4e9810bb6259bd9e9af837c91eb88dd079625
Garett Arrowood <garettarrowood@gmail.com>
2023-07-30 11:52:25 -0400

    Fix lint and mark broken test pending for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
-  validates :first_name, presence: true, length: { maximum: 50 }
-  validates :last_name, presence: true, length: { maximum: 50 }
-  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
-
+  validates :first_name, presence: true, length: {maximum: 50}
+  validates :last_name, presence: true, length: {maximum: 50}
+  validates :username, presence: true, uniqueness: true, length: {maximum: 50}
 end
