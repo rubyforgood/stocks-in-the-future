1f1b6e6e0c112c55a29d66601d1a89447ee22a57
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-30 10:17:35 -0400

    move stocks to TickerListComponent

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2023_07_29_152416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
