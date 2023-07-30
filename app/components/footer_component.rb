5dc940923fb69247a7f0a0d46571cecd86604245
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-30 09:39:42 -0400

    add footer tag, delete frozen string literals

--- app/components/footer_component.rb
+++ app/components/footer_component.rb
@@ -1,5 +1,2 @@
-# frozen_string_literal: true
-
 class FooterComponent < ViewComponent::Base
-
 end
--- app/components/ticker_list_component.rb
+++ app/components/ticker_list_component.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class TickerListComponent < ViewComponent::Base
   def initialize(stocks:)
     @stocks = stocks
