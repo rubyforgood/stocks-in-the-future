2e98d96ee0cdb167d87d5e0035123d3d8e733af4
kristenking <kristenking0110@gmail.com>
2023-07-30 10:51:04 -0400

    fix lint errors

--- db/migrate/20230729152416_devise_create_users.rb
+++ db/migrate/20230729152416_devise_create_users.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
@@ -34,7 +32,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       t.timestamps null: false
     end
-
     add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
