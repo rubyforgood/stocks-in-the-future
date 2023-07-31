d4e02ff23985b71e938183e5aaeb72a994648202
Randall Reed, Jr <randallreedjr@gmail.com>
2023-07-31 07:53:28 -0400

    Use implicit syntax for FactoryBot in specs

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe Cohort, type: :model do
   it "has a valid factory" do
-    expect(FactoryBot.build(:cohort)).to be_valid
+    expect(build(:cohort)).to be_valid
   end
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe School, type: :model do
   it "has a valid factory" do
-    expect(FactoryBot.build(:school)).to be_valid
+    expect(build(:school)).to be_valid
   end
 end
