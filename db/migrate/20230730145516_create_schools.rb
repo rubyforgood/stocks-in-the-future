6c9e39a9f2371813dec9026ebef524b8563cca21
kristenking <kristenking0110@gmail.com>
2023-08-11 13:43:36 -0400

    Add academic_year column to School

--- db/migrate/20230730145516_create_schools.rb
+++ db/migrate/20230730145516_create_schools.rb
@@ -2,7 +2,6 @@ class CreateSchools < ActiveRecord::Migration[7.0]
   def change
     create_table :schools do |t|
       t.text :name, null: false
-      t.integer :academic_year_id, null: false
 
       t.timestamps
     end
