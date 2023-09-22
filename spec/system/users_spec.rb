b035a999e72d9b33d78c49ed3a8f347d05ff1fcf
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-09-22 13:15:28 +0300

    57: Add more specs for sign_up flow

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Users", type: :system do
   end
 
   describe "sign up flow" do
-    xit "works" do
+    it "works" do
       visit "/users/sign_up"
 
       fill_in "Email", with: "test123@test.com"
@@ -14,6 +14,58 @@ RSpec.describe "Users", type: :system do
       fill_in "Password confirmation", with: "test123"
 
       expect { click_on "Sign up" }.to change(User, :count).by(1)
+    end
+
+    context "when the email input is not filled out" do
+      it "doesn't change the user count" do
+        visit "/users/sign_up"
+
+        fill_in "Email", with: ""
+        fill_in "Password", with: "test123"
+        fill_in "Password confirmation", with: "test123"
+
+        expect { click_on "Sign up" }.not_to change(User, :count)
+        expect(page).to have_content("Email can't be blank")
+      end
+    end
+
+    context "When the password input is not filled out" do
+      it "doesn't change the user count" do
+        visit "/users/sign_up"
+
+        fill_in "Email", with: "test123@test.com"
+        fill_in "Password", with: ""
+        fill_in "Password confirmation", with: "test123"
+
+        expect { click_on "Sign up" }.not_to change(User, :count)
+        expect(page).to have_content("Password can't be blank")
+      end
+    end
+
+    context "When the password confirmation input is not filled out" do
+      it "doesn't change the user count" do
+        visit "/users/sign_up"
+
+        fill_in "Email", with: "test123@test.com"
+        fill_in "Password", with: "test123"
+        fill_in "Password confirmation", with: ""
+
+        expect { click_on "Sign up" }.not_to change(User, :count)
+        expect(page).to have_content("Password confirmation doesn't match")
+      end
+
+      context "When all fields are not filled out" do
+        it "doesn't change the user count" do
+          visit "/users/sign_up"
+
+          fill_in "Email", with: ""
+          fill_in "Password", with: ""
+          fill_in "Password confirmation", with: ""
+
+          expect { click_on "Sign up" }.not_to change(User, :count)
+          expect(page).to have_content("Email can't be blank Password can't be blank")
+        end
+      end
     end
   end
 end
