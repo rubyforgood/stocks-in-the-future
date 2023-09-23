6b8e3605c929bf5fdad2e588b1426bec944d1347
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-09-23 15:00:33 +0300

    Configure action mailer for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Configure action mailer for the test environment
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 end
