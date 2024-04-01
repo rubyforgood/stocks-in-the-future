6fd7573a18164d25e273b76c24c7a56891ab6c15
Maksim Vakarchuk <11082252+maks112v@users.noreply.github.com>
2024-03-31 23:21:33 -0400

    adding username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_210728) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
+    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -51,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_210728) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "years", force: :cascade do |t|
