d1f5103ff26f36e56402393a0c7cc44633e363b5
Gabriela Tolosa Ramirez <46059184+ramirez-gabriela27@users.noreply.github.com>
2024-04-24 07:54:24 -0600

    adding models

---
+++ app/models/portfolio_stock.rb
@@ -0,0 +1,9 @@
+class PortfolioStock < ApplicationRecord
+  belongs_to :portfolio
+  belongs_to :stock
+
+  # Calculate the earnings of a single share of the stock
+  def calculate_earnings
+    # based on purchase price
+  end
+end
