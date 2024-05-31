92ae6250c185e9568e6acf812c0804f493c2c821
America Calleros <america.quintero@gmail.com>
2024-05-31 16:06:31 -0400

    Add students controller, test and view to display student cash balance

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :classrooms
   resources :schools
+  resources :students, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ test/controllers/students_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class StudentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @student = users(:one)
+  end
+
+  test 'should show student' do
+    get student_url(@student)
+    assert_response :success
+  end
+end
