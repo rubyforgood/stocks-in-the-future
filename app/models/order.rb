2919e897cbc79298cb6649c7a8827f492e0dbab1
America Calleros <america.quintero@gmail.com>
2024-06-01 14:21:24 -0400

    Add pending scope to order model and add test

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :stock
 
-  enum status: { pending: 0, completed: 1, canceled: 2 }
+  enum status: {pending: 0, completed: 1, canceled: 2}
+
+  scope :pending, -> { where(status: :pending) }
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -10,4 +10,12 @@ class OrderTest < ActiveSupport::TestCase
 
     assert order.save
   end
+
+  test "can filter orders by pending status" do
+    student = Student.first
+    pending_orders = [Order.create(stock: Stock.first, shares: 5, status: :pending, user: student)]
+    Order.create(stock: Stock.first, shares: 5, status: :completed, user: student)
+
+    assert_equal pending_orders, Order.pending
+  end
 end
