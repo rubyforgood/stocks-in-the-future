da48851bcb7309b726667dceaabc094cea9c9759
yinho999 <yinho999@gmail.com>
2025-05-26 13:41:53 +0100

    Add unique email index and update user factory to prevent duplication. (#241)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_22_020802) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_25_174026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_22_020802) do
     t.bigint "classroom_id"
     t.boolean "admin", default: false
     t.index ["classroom_id"], name: "index_users_on_classroom_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   test "factory" do
     assert build(:user).validate!
   end
+
+  test "validate uniqueness of email" do
+    create(:user, email: "test@example.com")
+    new_user = build(:user, email: "test@example.com")
+
+    assert_not new_user.valid?
+    assert_includes new_user.errors[:email], "has already been taken"
+  end
 end
