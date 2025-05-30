b5911b03ebb8f7d867617cc0f26ef78e22b42322
Naiker <yinho999@gmail.com>
2025-05-30 18:08:51 +0100

    Fix error

--- test/factories/stocks.rb
+++ test/factories/stocks.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
   factory :stock do
     sequence(:ticker) { |n| "STOCK#{n}" }
     stock_exchange { "NYSE" }
-    company_name { |n| "Sample Company#{n} Inc." }
+    sequence(:company_name) { |n| "Sample Company#{n} Inc." }
     company_website { "https://example.com" }
     description { "A sample stock for testing" }
     industry { "Technology" }
     management { "CEO: John Doe" }
     employees { 1000 }
-    competitor_names { |n| "Competitor #{n}, Competitor #{n + 1}" }
+    sequence(:competitor_names) { |n| "Competitor #{n}, Competitor #{n + 1}" }
     sales_growth { 5.5 }
     industry_avg_sales_growth { 4.2 }
     debt_to_equity { 0.3 }
