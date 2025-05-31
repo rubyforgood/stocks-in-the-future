24ab25ba40c59743897d78f6ba15f460a029e8bd
Naiker <yinho999@gmail.com>
2025-05-31 18:08:21 +0100

    Refactor StocksController and associated tests for improved readability and consistency.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,12 +4,10 @@ class StocksController < ApplicationController
   before_action :set_stock, only: %i[show]
   before_action :authenticate_user!
 
-  # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
   end
 
-  # GET /stocks/1 or /stocks/1.json
   def show
   end
 
--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -2,18 +2,21 @@ require "test_helper"
 
 class StocksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    @stock = create(:stock)
-    @user = create(:user)
-    sign_in @user
+    user = create(:user)
+    sign_in user
+
     get stocks_url
+
     assert_response :success
   end
 
   test "should get show" do
-    @stock = create(:stock)
-    @user = create(:user)
-    sign_in @user
-    get stock_url(@stock)
+    stock = create(:stock)
+    user = create(:user)
+    sign_in user
+
+    get stock_url(stock)
+
     assert_response :success
   end
 end
