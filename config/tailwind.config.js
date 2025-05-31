cfa6467b890809ecba82586003f5a38ede055890
Abdullah Hashim <abdul@hey.com>
2025-05-31 01:40:42 -0400

    Migrate to Propshaft and Node-free Tailwind setup

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -88,11 +88,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/container-queries"),
-    require("tailwindcss-animate"),
-  ],
 };
