bd1a39a4ff98609fd1aac1293beffbcb566bbe9d
Naiker <yinho999@gmail.com>
2025-06-02 17:34:26 +0100

    Render classroom students table in the Classroom view and preload associated data to avoid n+1 queries.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -55,7 +55,7 @@ class ClassroomsController < ApplicationController
   private
 
   def set_classroom
-    @classroom = Classroom.find(params[:id])
+    @classroom = Classroom.includes(users: :portfolio).find(params[:id].to_i)
   end
 
   def classroom_params
