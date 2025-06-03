5878b94c70224b2713033aaedece5e4e6c02dc1e
Polly Sileo <pollysileo@gmail.com>
2025-06-02 21:53:46 -0400

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,16 @@ const shadcnConfig = require('./config/shadcn.tailwind.js');
 
 module.exports = {
   ...shadcnConfig,
+  theme: {
+    ...shadcnConfig.theme,
+    extend: {
+      ...shadcnConfig.theme.extend,
+      colors: {
+        ...shadcnConfig.theme.extend.colors,
+        'primary-color': {
+          DEFAULT: '#00698C',
+        },
+      },
+    },
+  },
 };
