a56a0bb16f47e48cc4cf1a46e84d1aa88768f655
Paul Sandhu <spaulsandhu@gmail.com>
2025-07-03 12:12:30 -0700

    Update data model
    This includes two changes to our data model:
    
    1) Renaming of a column on the GradeEntry model. Attendance will be represented by "perfect_weeks" vs "days_missed"
    
    2) Switch from dependent destroy to nullify for the relation between Classroom and GradeBook

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -6,5 +6,5 @@ class Classroom < ApplicationRecord
   has_one :year, through: :school_year
 
   has_many :users, dependent: :nullify
-  has_many :grade_books, dependent: :destroy
+  has_many :grade_books, dependent: :nullify
 end
--- app/models/grade_entry.rb
+++ app/models/grade_entry.rb
@@ -3,6 +3,4 @@
 class GradeEntry < ApplicationRecord
   belongs_to :grade_book
   belongs_to :user
-
-  validates :days_missed, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
