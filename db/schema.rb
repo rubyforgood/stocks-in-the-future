3d286d12aa7a8058f5de6e6e5baddd1053d52b6a
Domenick Powers <dmnckpwrs@gmail.com>
2025-07-03 15:02:46 -0400

    Bad merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[8.0].define(version: 2025_07_03_151816) do
-=======
-ActiveRecord::Schema[8.0].define(version: 2025_07_03_151425) do
->>>>>>> main
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
