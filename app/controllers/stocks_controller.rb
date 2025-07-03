859467e1049a5b7006e06dde21f8a2fdb5a72f0a
America Calleros <america.quintero@gmail.com>
2025-07-03 10:21:39 -0500

    Only show cash_balance to students

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,7 @@
 
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[show]
+  before_action :set_portfolio, only: %i[index show], if: :user_is_student?
   before_action :authenticate_user!
 
   def index
@@ -12,7 +13,15 @@ class StocksController < ApplicationController
 
   private
 
+  def set_portfolio
+    @portfolio = current_user.portfolio
+  end
+
   def set_stock
     @stock = Stock.find(params[:id])
+  end
+
+  def user_is_student?
+    current_user.student?
   end
 end
