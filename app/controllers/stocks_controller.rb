10abc1183a3b3ec2bc12ffdf3813878bf306cb75
America Calleros <america.quintero@gmail.com>
2025-07-03 10:36:27 -0500

    Add stock tickers to seed file

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@
 
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[show]
-  before_action :set_portfolio, only: %i[index show], if: :user_is_student?
+  before_action :set_portfolio
   before_action :authenticate_user!
 
   def index
@@ -19,9 +19,5 @@ class StocksController < ApplicationController
 
   def set_stock
     @stock = Stock.find(params[:id])
-  end
-
-  def user_is_student?
-    current_user.student?
   end
 end
