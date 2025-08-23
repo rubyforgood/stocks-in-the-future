26bcba1edb65ff499ab10d05d80cb9492051c376
Polly Sileo <pollysileo@gmail.com>
2025-08-23 12:46:31 -0400

    Update index page to table and remove show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[show edit update destroy]
+  before_action :set_order, only: %i[edit update destroy cancel]
   before_action :set_stock, only: %i[new]
   before_action :set_shared_owned, only: %i[new edit]
   before_action :authenticate_user!
@@ -9,8 +9,6 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
-
-  def show; end
 
   def new
     @order = Order.new(transaction_type: params[:transaction_type], stock: @stock)
@@ -23,7 +21,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to order_url(@order), notice: t(".notice") }
+        format.html { redirect_to orders_url, notice: t(".notice") }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -35,11 +33,36 @@ class OrdersController < ApplicationController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: t(".notice") }
+        format.html { redirect_to orders_url, notice: t(".notice") }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def cancel
+    # Authorization: ensure user can only cancel their own orders
+    if @order.user != current_user
+      respond_to do |format|
+        format.html { redirect_to orders_url, alert: "You can only cancel your own orders" }
+        format.json { render json: { error: "You can only cancel your own orders" }, status: :forbidden }
+      end
+      return
+    end
+
+    # Validation: only pending orders can be canceled
+    if @order.pending?
+      @order.update(status: :canceled)
+      respond_to do |format|
+        format.html { redirect_to orders_url, notice: "Order was successfully canceled" }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to orders_url, alert: "Only pending orders can be canceled" }
+        format.json { render json: { error: "Only pending orders can be canceled" }, status: :unprocessable_entity }
       end
     end
   end
