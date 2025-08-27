544348bb9a116ba85d60d03f57b112218323f435
Polly Sileo <pollysileo@gmail.com>
2025-08-26 22:59:34 -0400

    linting

--- app/jobs/stock_prices_update_job.rb
+++ app/jobs/stock_prices_update_job.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 class StockPricesUpdateJob < ApplicationJob
   queue_as :default
 
+  # rubocop:disable Metrics/AbcSize
   def perform(...)
     # For each stock symbol, request the latest closing cost
     # update the stocks table with each new closing cost
     Stock::SYMBOLS.each do |symbol|
       Rails.logger.info "Updating stock price for #{symbol}"
-      
+
       data = api_request(symbol)
-      
+
       # Check if API returned valid data
       price = data&.dig("Global Quote", "05. price")
       unless price
@@ -21,14 +22,14 @@ class StockPricesUpdateJob < ApplicationJob
         next
       end
       Rails.logger.info "API returned price #{price} for #{symbol}"
-      
+
       stock = Stock.find_or_initialize_by(ticker: symbol)
       old_price = stock.price_cents
-      
+
       # Convert price to cents
       stock.price_cents = (price.to_f * 100).to_i
       Rails.logger.info "Converting #{symbol} price from #{old_price} to #{stock.price_cents} cents"
-      
+
       # Save with error handling
       if stock.save
         Rails.logger.info "Successfully updated #{symbol} price to $#{stock.current_price}"
@@ -37,6 +38,8 @@ class StockPricesUpdateJob < ApplicationJob
       end
     end
   end
+
+  # rubocop:enable Metrics/AbcSize
 
   private
 
