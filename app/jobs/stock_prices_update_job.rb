0f13d5bd75fffed30ccd6b2ba449646551fb7490
Polly Sileo <pollysileo@gmail.com>
2025-08-26 22:53:42 -0400

    Add rails loggers for debugging

--- app/jobs/stock_prices_update_job.rb
+++ app/jobs/stock_prices_update_job.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'json'
+
 class StockPricesUpdateJob < ApplicationJob
   queue_as :default
 
@@ -7,12 +10,31 @@ class StockPricesUpdateJob < ApplicationJob
     # For each stock symbol, request the latest closing cost
     # update the stocks table with each new closing cost
     Stock::SYMBOLS.each do |symbol|
+      Rails.logger.info "Updating stock price for #{symbol}"
+      
       data = api_request(symbol)
-      price = data["Global Quote"]["05. price"]
+      
+      # Check if API returned valid data
+      price = data&.dig("Global Quote", "05. price")
+      unless price
+        Rails.logger.error "Invalid API response for #{symbol}: #{data}"
+        next
+      end
+      Rails.logger.info "API returned price #{price} for #{symbol}"
+      
       stock = Stock.find_or_initialize_by(ticker: symbol)
-      # TODO: What's returned from the API? We likely need to normalize this.
+      old_price = stock.price_cents
+      
+      # Convert price to cents
       stock.price_cents = (price.to_f * 100).to_i
-      stock.save
+      Rails.logger.info "Converting #{symbol} price from #{old_price} to #{stock.price_cents} cents"
+      
+      # Save with error handling
+      if stock.save
+        Rails.logger.info "Successfully updated #{symbol} price to $#{stock.current_price}"
+      else
+        Rails.logger.error "Failed to save #{symbol}: #{stock.errors.full_messages}"
+      end
     end
   end
 
