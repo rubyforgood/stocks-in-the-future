3f9e97ca8a35c84a16e8b1f38c5c0dc55c53d6a1
Polly Sileo <pollysileo@gmail.com>
2025-09-03 21:53:47 -0400

    cleanup

--- test/models/sti_test.rb
+++ test/models/sti_test.rb
@@ -4,31 +4,24 @@ require "test_helper"
 
 class StiTest < ActiveSupport::TestCase
   def test_sti_implementation
-    # Create a classroom for the users
     classroom = create(:classroom)
 
-    # Test creating an Admin
     admin = create(:admin, classroom: classroom)
     assert_equal "User", admin.type
 
-    # Test creating a Student
     student = create(:student, classroom: classroom)
     assert_equal "Student", student.type
 
-    # Test creating a Teacher
     teacher = create(:teacher, classroom: classroom)
     assert_equal "Teacher", teacher.type
 
-    # Test updating an Admin to be a Student
     admin.update(type: "Student")
     assert_equal "Student", admin.type
 
-    # Verify that the records are saved correctly
     assert_equal 3, User.count
     assert_equal 2, Student.count
     assert_equal 1, Teacher.count
 
-    # Verify that the type field is set correctly
     assert_equal "Student", admin.reload.type
     assert_equal "Student", student.reload.type
     assert_equal "Teacher", teacher.reload.type
--- test/services/purchase_stock_test.rb
+++ test/services/purchase_stock_test.rb
@@ -67,7 +67,6 @@ class PurchaseStockTest < ActiveSupport::TestCase
     stock = create(:stock, price_cents: 100)
     order = create(:order, :pending, :buy, shares: 5, stock: stock, user: portfolio.user)
 
-    # Mock PortfolioStock creation to fail
     PortfolioStock.any_instance.stubs(:save!).raises(ActiveRecord::RecordInvalid.new(PortfolioStock.new))
 
     assert_raises(ActiveRecord::RecordInvalid) do
@@ -77,8 +76,6 @@ class PurchaseStockTest < ActiveSupport::TestCase
     order.reload
 
     assert_equal "pending", order.status
-    # Since we removed the after_create callback, portfolio_transaction is only created by the service
-    # When the service fails, no portfolio_transaction should be created
     assert_nil order.portfolio_transaction
     assert_nil order.portfolio_stock
   end
@@ -114,21 +111,22 @@ class PurchaseStockTest < ActiveSupport::TestCase
     assert_equal stock, sell_record.stock
   end
 
-  # test "multiple buy orders create separate portfolio_stock records" do
-  #   portfolio = create(:portfolio)
-  #   portfolio.portfolio_transactions.create!(amount_cents: 1000, transaction_type: :deposit)
-  #   stock = create(:stock, price_cents: 100)
+  test "multiple buy orders create separate portfolio_stock records" do
+    skip ("test is failing and needs fixing")
+    portfolio = create(:portfolio)
+    portfolio.portfolio_transactions.create!(amount_cents: 1000, transaction_type: :deposit)
+    stock = create(:stock, price_cents: 100)
 
-  #   order1 = create(:order, :pending, :buy, shares: 5, stock: stock, user: portfolio.user)
-  #   PurchaseStock.execute(order1)
+    order1 = create(:order, :pending, :buy, shares: 5, stock: stock, user: portfolio.user)
+    PurchaseStock.execute(order1)
 
-  #   order2 = create(:order, :pending, :buy, shares: 3, stock: stock, user: portfolio.user)
-  #   PurchaseStock.execute(order2)
+    order2 = create(:order, :pending, :buy, shares: 3, stock: stock, user: portfolio.user)
+    PurchaseStock.execute(order2)
 
-  #   portfolio_stocks = PortfolioStock.where(stock: stock, portfolio: portfolio)
-  #   assert_equal 2, portfolio_stocks.count
-  #   assert_equal [3, 5], portfolio_stocks.pluck(:shares).sort
-  # end
+    portfolio_stocks = PortfolioStock.where(stock: stock, portfolio: portfolio)
+    assert_equal 2, portfolio_stocks.count
+    assert_equal [3, 5], portfolio_stocks.pluck(:shares).sort
+  end
 
   test "buy then sell orders work together correctly" do
     portfolio = create(:portfolio)
