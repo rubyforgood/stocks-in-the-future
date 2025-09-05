b714aa9dad0221a9ac845fefb24bfe0f09e83bd6
David Gero <davidpgero@gmail.com>
2025-09-05 13:42:03 +0200

    441 adding more dynamic year seed data + production seed
    It resolves https://github.com/rubyforgood/stocks-in-the-future/issues/441

--- db/seeds/partials/years.rb
+++ db/seeds/partials/years.rb
@@ -1,7 +1,8 @@
-start_year = Date.current.year
-end_year = start_year + 12
+current_year = Date.current.year
+start_year = current_year - 3
+end_year = current_year + 10
 
-(start_year...end_year).each do |i|
+(start_year..end_year).each do |i|
   year = Year.find_or_create_by(name: "#{i} - #{i + 1}")
   year.save!
 end
