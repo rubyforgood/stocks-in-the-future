b9295b35c4c834b971180a3a2126e80759275934
Sean Dickinson <sean@bythepixel.com>
2025-09-12 15:18:39 -0400

    feat: grade entry based awards

--- app/models/grade_entry.rb
+++ app/models/grade_entry.rb
@@ -4,9 +4,28 @@ class GradeEntry < ApplicationRecord
   belongs_to :grade_book
   belongs_to :user
 
+  PER_DAY_ATTENDANCE_AWARD = 20
+  AWARD_FOR_A_GRADE = 3_00
+  AWARD_FOR_B_GRADE = 2_00
+
   GRADE_OPTIONS = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F"].freeze
 
-  def finalizable?
-    math_grade.present? && reading_grade.present? && attendance_days.present?
+  def finalizable? = math_grade.present? && reading_grade.present? && attendance_days.present?
+  def award_for_attendance = (attendance_days || 0) * PER_DAY_ATTENDANCE_AWARD
+  def award_for_reading = grade_based_award(reading_grade)
+  def award_for_math = grade_based_award(math_grade)
+  def total_award = award_for_attendance + award_for_reading + award_for_math
+
+  private
+
+  def grade_based_award(grade)
+    case grade
+    when "A+", "A", "A-"
+      AWARD_FOR_A_GRADE
+    when "B+", "B", "B-"
+      AWARD_FOR_B_GRADE
+    else
+      0
+    end
   end
 end
--- test/models/grade_entry_test.rb
+++ test/models/grade_entry_test.rb
@@ -36,4 +36,58 @@ class GradeEntryTest < ActiveSupport::TestCase
     entry.attendance_days = 30
     assert entry.finalizable?
   end
+
+  test "#award_for_attendance is 20 cents per day" do
+    entry = build(:grade_entry, attendance_days: 0)
+    assert_equal 0, entry.award_for_attendance
+
+    entry.attendance_days = 5
+    assert_equal 100, entry.award_for_attendance
+
+    entry.attendance_days = 12
+    assert_equal 240, entry.award_for_attendance
+  end
+
+  test "#award_for_reading grade is $2 for B, $3 for an A" do
+    entry = build(:grade_entry, reading_grade: nil)
+
+    ["F", "D", "C-", "C", "C+"].each do |grade|
+      entry.reading_grade = grade
+      assert_equal 0, entry.award_for_reading
+    end
+
+    ["B-", "B", "B+"].each do |grade|
+      entry.reading_grade = grade
+      assert_equal 2_00, entry.award_for_reading
+    end
+
+    ["A-", "A", "A+"].each do |grade|
+      entry.reading_grade = grade
+      assert_equal 3_00, entry.award_for_reading
+    end
+  end
+
+  test "#award_for_math grade is $2 for B, $3 for an A" do
+    entry = build(:grade_entry, math_grade: nil)
+
+    ["F", "D", "C-", "C", "C+"].each do |grade|
+      entry.math_grade = grade
+      assert_equal 0, entry.award_for_math
+    end
+
+    ["B-", "B", "B+"].each do |grade|
+      entry.math_grade = grade
+      assert_equal 2_00, entry.award_for_math
+    end
+
+    ["A-", "A", "A+"].each do |grade|
+      entry.math_grade = grade
+      assert_equal 3_00, entry.award_for_math
+    end
+  end
+
+  test "#total_award sums attendance, reading, and math awards" do
+    entry = build(:grade_entry, math_grade: "A", reading_grade: "B", attendance_days: 9)
+    assert_equal 3_00 + 2_00 + (9 * 20), entry.total_award
+  end
 end
