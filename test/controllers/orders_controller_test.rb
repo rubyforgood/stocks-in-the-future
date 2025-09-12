378bc42fbcd57c25fca27241a8af3c55c8b14e33
Sean Dickinson <sean@bythepixel.com>
2025-09-12 09:55:04 -0400

    feat: add controller test

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -33,6 +33,9 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
       post(orders_path, params:)
     end
 
+    new_order = Order.last
+    assert_equal Order::TRANSACTION_FEE_CENTS, new_order.transaction_fee_cents
+
     assert_redirected_to orders_path
   end
 
@@ -84,6 +87,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to orders_path
     assert order.shares, 3
+    assert order.transaction_fee_cents, Order::TRANSACTION_FEE_CENTS
   end
 
   # TODO: Add test for update with invalid params
