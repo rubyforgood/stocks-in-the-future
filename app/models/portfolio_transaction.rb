63e39bb42c24577b42f3776db1b5e8ca0961b13f
Sean Dickinson <sean@bythepixel.com>
2025-09-13 14:39:40 -0400

    feat: add fee

--- app/models/portfolio_transaction.rb
+++ app/models/portfolio_transaction.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PortfolioTransaction < ApplicationRecord
+  TRANSACTION_FEE_CENTS = 1_00
   # deposit/witdrawal is for cash transactions ie grades and attendance
   # credit/debit is for stock transactions ie buy/sell stocks
   enum :transaction_type, { deposit: 0, withdrawal: 1, credit: 2, debit: 3, fee: 4 }
