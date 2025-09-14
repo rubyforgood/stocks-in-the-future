fa34191a904552905ed9f9bb698fd6956a708ca3
Polly Sileo <pollysileo@gmail.com>
2025-09-14 19:27:24 -0400

    Rubocop Fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
 
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_option|
-    driver_option.add_argument('--no-sandbox')
-    driver_option.add_argument('--disable-dev-shm-usage')
+    driver_option.add_argument("--no-sandbox")
+    driver_option.add_argument("--disable-dev-shm-usage")
   end
 end
