6e4e05df0adaf3e762b564cd81aa1d8be278ef71
Cornelio Garcia <cornelio.garciajr@icloud.com>
2025-09-30 13:29:24 -0500

    default users to root path on login

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,35 +3,34 @@
 require "test_helper"
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test "after_sign_in_path_for redirects students to portfolio" do
+  test "after_sign_in_path_for redirects students to root" do
     student = create(:student, username: "teststudent", password: "password123")
-    portfolio = student.portfolio
 
     post user_session_path, params: {
       user: { username: "teststudent", password: "password123" }
     }
 
-    assert_redirected_to user_portfolio_path(student, portfolio)
+    assert_redirected_to root_path
   end
 
-  test "after_sign_in_path_for redirects teachers to classrooms" do
+  test "after_sign_in_path_for redirects teachers to root" do
     create(:teacher, username: "testteacher", password: "password123")
 
     post user_session_path, params: {
       user: { username: "testteacher", password: "password123" }
     }
 
-    assert_redirected_to classrooms_path
+    assert_redirected_to root_path
   end
 
-  test "after_sign_in_path_for redirects admins to classrooms" do
+  test "after_sign_in_path_for redirects admins to root" do
     create(:admin, username: "testadmin", password: "password123")
 
     post user_session_path, params: {
       user: { username: "testadmin", password: "password123" }
     }
 
-    assert_redirected_to classrooms_path
+    assert_redirected_to root_path
   end
 
   test "ensure_teacher_or_admin allows teachers" do
