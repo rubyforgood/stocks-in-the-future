7378a0da372e8b033d7ed628e8d55630150698fa
Joel Shin <jwshin@gmail.com>
2025-10-01 10:25:08 -0700

    Issue-660: create next and previous for year model
    We'd like for a quarter to be smarter and understand previous and next to sequence
    across years. To support that, we need to make years smarter in the same way. That way
    we can ensure that traversing relationships from quarter to school_year to year -- just
    works.
    
    See Issue-660 for architectural design analysis.

--- test/models/year_test.rb
+++ test/models/year_test.rb
@@ -22,4 +22,19 @@ class YearTest < ActiveSupport::TestCase
     assert_not year.valid?
     assert year.errors.added?(:name, :taken, value: duplicate_year)
   end
+
+  test "previous_year and next_year" do
+    year_2022_2023 = create(:year, name: "2022 - 2023")
+    year_2023_2024 = create(:year, name: "2023 - 2024")
+    year_2024_2025 = create(:year, name: "2024 - 2025")
+
+    assert_nil year_2022_2023.previous_year
+    assert_equal year_2023_2024, year_2022_2023.next_year
+
+    assert_equal year_2022_2023, year_2023_2024.previous_year
+    assert_equal year_2024_2025, year_2023_2024.next_year
+
+    assert_equal year_2023_2024, year_2024_2025.previous_year
+    assert_nil year_2024_2025.next_year
+  end
 end
