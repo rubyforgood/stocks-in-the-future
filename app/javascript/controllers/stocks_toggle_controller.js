a869f79d00acf94136febd168f4083069f900504
David Gero <davidpgero@gmail.com>
2025-10-04 18:05:47 +0200

    remove duplicated controller

--- app/javascript/controllers/stocks_toggle_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  navigateLink(event) {
-    // Prevent the summary from toggling when clicking the link
-    event.stopPropagation()
-    // Let the link navigate normally
-  }
-}
