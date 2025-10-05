b5ada66d3d4b5e3c25d9cffffa5bdf9ec8047952
suraj-mandal <suraj.mandal738@gmail.com>
2025-10-05 19:01:48 +0530

    Added tests for navigation

---
+++ test/system/admin/admin_navigation_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+module Admin
+  class AdminNavigationTest < ApplicationSystemTestCase
+    setup do
+      @admin = create(:admin)
+      sign_in @admin
+    end
+
+    test "admin navigation does not show School Years link" do
+      visit admin_root_path
+      assert_no_link "School Years"
+
+      assert_link "Announcements"
+      assert_link "Classrooms"
+      assert_link "Schools"
+      assert_link "Stocks"
+      assert_link "Students"
+      assert_link "Teachers"
+      assert_link "Users"
+      assert_link "Years"
+    end
+  end
+end
