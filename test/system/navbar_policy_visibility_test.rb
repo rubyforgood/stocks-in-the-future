1b71781e1abd22f027b280c5a5db0ad8f2d897c6
Polly Schandorf <pollygee@users.noreply.github.com>
2025-10-07 19:26:38 -0400

    Remove inactive stocks from navbar (#725)
    * Remove inactive stocks from navbar
    
    * Fix lint

--- test/system/navbar_policy_visibility_test.rb
+++ test/system/navbar_policy_visibility_test.rb
@@ -31,4 +31,44 @@ class NavbarPolicyVisibilityTest < ActionDispatch::IntegrationTest
     get root_path
     assert_select "a", text: "Admin"
   end
+
+  test "navbar shows only active stocks" do
+    active_stock = create(:stock, archived: false, ticker: "AAPL")
+    archived_stock = create(:stock, archived: true, ticker: "DEAD")
+
+    sign_in @student
+    get root_path
+
+    assert_select "p", text: active_stock.ticker
+    assert_select "p", text: archived_stock.ticker, count: 0
+  end
+
+  test "navbar stocks are displayed in trading floor dropdown" do
+    active_stock1 = create(:stock, archived: false, ticker: "AAPL")
+    active_stock2 = create(:stock, archived: false, ticker: "GOOGL")
+    archived_stock = create(:stock, archived: true, ticker: "DEAD")
+
+    sign_in @student
+    get root_path
+
+    assert_select "details[data-controller='stock-navbar-toggle']" do
+      assert_select "p", text: active_stock1.ticker
+      assert_select "p", text: active_stock2.ticker
+    end
+
+    assert_select "details[data-controller='stock-navbar-toggle']" do
+      assert_select "p", text: archived_stock.ticker, count: 0
+    end
+  end
+
+  test "navbar stocks link to correct stock paths" do
+    active_stock = create(:stock, archived: false, ticker: "AAPL")
+
+    sign_in @student
+    get root_path
+
+    assert_select "a[href='#{stock_path(active_stock)}']" do
+      assert_select "p", text: active_stock.ticker
+    end
+  end
 end
