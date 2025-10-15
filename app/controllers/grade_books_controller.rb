7bb1add63345ac21f3e63dcc0585a9f9f5cf396b
Barsu5489 <ebarsulai@gmail.com>
2025-10-15 22:59:42 +0300

    Block grade book access for archived classrooms

--- app/controllers/grade_books_controller.rb
+++ app/controllers/grade_books_controller.rb
@@ -43,6 +43,11 @@ class GradeBooksController < ApplicationController
   def set_classroom_and_grade_book
     @classroom = Classroom.find(params[:classroom_id])
     @grade_book = @classroom.grade_books.includes(grade_entries: :user).find(params[:id])
+
+    # Redirect if classroom is archived and user is not an admin
+    if @classroom.archived? && !current_user.admin?
+      redirect_to root_path, alert: "This classroom has been archived and is no longer accessible."
+    end
   end
 
   def grade_entry_params
