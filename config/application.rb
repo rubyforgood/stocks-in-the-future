d1b94678551c9c45db6fc0f8955a2396ae746cde
Polly Sileo <pollysileo@gmail.com>
2025-10-15 00:08:04 -0400

    Setup for action text so annoucments will work in heroku

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,9 @@ require_relative "boot"
 
 require "rails/all"
 
+# Explicitly require Action Text to ensure it's available
+require "action_text/engine"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
