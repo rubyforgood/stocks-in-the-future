38a25eeba83b25f0e0a50707083fa08f6a0569dd
Barsu5489 <ebarsulai@gmail.com>
2025-11-09 00:09:37 +0300

    id validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,7 @@
 class Student < User
   has_many :student_classrooms, dependent: :destroy
   has_many :classrooms, through: :student_classrooms
+  validate :classroom_must_exist
 
   # Ensure students have nil email by default (not empty string)
   after_initialize :set_default_email, if: :new_record?
@@ -18,5 +19,11 @@ class Student < User
 
   def ensure_portfolio
     create_portfolio!(current_position: 0) if portfolio.blank?
+  end
+
+  def classroom_must_exist
+    return if classroom_id.blank?
+
+    errors.add(:classroom, "can't be blank") unless Classroom.exists?(classroom_id)
   end
 end
