8a94b90a7adbed96cff014479bc11e61fc2d1663
Barsu5489 <ebarsulai@gmail.com>
2025-11-13 03:58:48 +0300

    remove unused controller

--- app/models/student_classroom.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class StudentClassroom < ApplicationRecord
-  belongs_to :student, class_name: "Student"
-  belongs_to :classroom
-
-  validates :student_id, uniqueness: { scope: :classroom_id }
-end
