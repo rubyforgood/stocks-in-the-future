d23046da3286d6dd04504d208ba0eb83bcd811f6
Barsu5489 <ebarsulai@gmail.com>
2025-11-14 06:16:41 +0300

    add class validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Student < User
+  validates :classroom, presence: true
   has_many :enrollments, dependent: :destroy
   has_many :classrooms, through: :enrollments
 
