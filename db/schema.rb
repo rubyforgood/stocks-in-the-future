f4e9eba435a29364cb97c4315c10cc76044c2044
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-12-04 22:49:24 +0900

    Add featured boolean column to announcements table
    - Add migration to add featured column with default false
    - Update schema.rb to reflect new column
    - Column is not null with default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_16_135149) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_04_134708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_16_135149) do
   create_table "announcements", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
+    t.boolean "featured", default: false, null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_announcements_on_created_at", order: :desc
