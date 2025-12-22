2688bdf0a61a29feaa05d52b5a23cfdf916a3e73
America Calleros <america.quintero@gmail.com>
2025-12-22 17:05:05 -0600

    Ensure old admin page still functions while new admin pages are built

---
+++ app/controllers/admin_v2/base_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Base controller for Admin V2 (in-house admin)
+# All AdminV2 controllers inherit from this controller.
+module AdminV2
+  class BaseController < ApplicationController
+    include Pundit::Authorization
+
+    layout "admin_v2"
+
+    before_action :authenticate_admin
+
+    private
+
+    def authenticate_admin
+      redirect_to root_path, alert: t("application.access_denied.admin_required") unless current_user&.admin?
+    end
+  end
+end
