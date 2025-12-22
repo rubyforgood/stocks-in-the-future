4e2044d87a87745bf9aa5107fced476cda0638fb
America Calleros <america.quintero@gmail.com>
2025-12-22 13:41:44 -0600

    [#924] Add v2 admin controller and route

---
+++ app/controllers/admin/v2/dashboard_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Admin
+  module V2
+    class DashboardController < BaseController
+      def index
+        # Dashboard will show overview of admin resources
+      end
+    end
+  end
+end
