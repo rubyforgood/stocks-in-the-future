7b46aa9cbc9f450f0c0f6a134088cd87864ef7ad
David Gerő <davidpgero@gmail.com>
2026-01-19 01:12:44 +0100

    fixing main, minitest was not compatible with rails 8 (#996)
    * fixing main, minitest was not compatible with rails 8
    
    * fixing linter issue
    
    * fixing seed data

--- db/seeds/partials/classrooms.rb
+++ db/seeds/partials/classrooms.rb
@@ -1,3 +1,7 @@
-classroom = Classroom.find_or_create_by!(name: "Smith's Sixth Grade", school_year: SchoolYear.first, trading_enabled: true)
-grade = Grade.find_by(level: 6)
-classroom.grades << grade if grade && !classroom.grades.include?(grade)
+# frozen_string_literal: true
+
+grade = Grade.find_by!(level: 6)
+classroom = Classroom.find_or_initialize_by(name: "Smith's Sixth Grade", school_year: SchoolYear.first)
+classroom.trading_enabled = true
+classroom.grades << grade unless classroom.grades.include?(grade)
+classroom.save!
