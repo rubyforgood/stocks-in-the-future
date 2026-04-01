43f009e94b5152c284f1fb3acf01207c9dc844eb
Walter McCone <walter@careerplug.com>
2026-04-01 15:25:13 -0500

    Move quarter creation into a callback

--- app/models/school_year.rb
+++ app/models/school_year.rb
@@ -6,10 +6,20 @@ class SchoolYear < ApplicationRecord
   has_many :classrooms, dependent: :restrict_with_error
   has_many :quarters, dependent: :restrict_with_error
 
+  after_create :create_quarters
+
   delegate :name, to: :school, prefix: :school
   delegate :name, to: :year, prefix: :year
 
   def name
     "#{school_name} (#{year_name})"
+  end
+
+  private
+
+  def create_quarters
+    (1..4).each do |n|
+      quarters.create!(name: "Quarter #{n}", number: n)
+    end
   end
 end
